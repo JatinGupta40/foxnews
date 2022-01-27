92ea02c322a3da6188ef8e075be4b27d5607a42b
JatinGupta40 <gupta.jatin40@gmail.com>
2022-01-28 00:08:06 +0530

    #2-footer-complete-with-responsive

---
+++ web/sites/default/files/php/twig/61f2e2c26fb5f_menu--extras.html.twig_o_ApavWJtQeUTu_xkFkE9zuNV/nmL0OmQLLpvkbgxoc0L7yty0VOYGQH1lKZF_BYLnNJA.php
@@ -0,0 +1,126 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/menu_item_extras/modules/mie_demo_base/templates/menu--extras.html.twig */
+class __TwigTemplate_6f539c39dfd08506136818393633aea3c9ade0fd627d3a221545c4f2ecdc81fb extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 23
+        return "menu-levels.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453 = $this->extensions["Drupal\\webprofiler\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453->enter($__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "modules/menu_item_extras/modules/mie_demo_base/templates/menu--extras.html.twig"));
+
+        // line 24
+        $context["elements"] = ($context["items"] ?? null);
+        // line 23
+        $this->parent = $this->loadTemplate("menu-levels.html.twig", "modules/menu_item_extras/modules/mie_demo_base/templates/menu--extras.html.twig", 23);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453->leave($__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/menu_item_extras/modules/mie_demo_base/templates/menu--extras.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  48 => 23,  46 => 24,  36 => 23,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Theme override to display a menu.
+ *
+ * Available variables:
+ * - menu_name: The machine name of the menu.
+ * - items: A nested list of menu items. Each menu item contains:
+ *   - attributes: HTML attributes for the menu item.
+ *   - content.children: The menu item child items.
+ *   - title: The menu link title.
+ *   - url: The menu link url, instance of \\Drupal\\Core\\Url
+ *   - content: The field item content.
+ *   - localized_options: Menu link localized options.
+ *   - is_expanded: TRUE if the link has visible children within the current
+ *     menu tree.
+ *   - is_collapsed: TRUE if the link has children within the current menu tree
+ *     that are not currently visible.
+ *   - in_active_trail: TRUE if the link is in the active trail.
+ */
+#}
+
+{% extends \"menu-levels.html.twig\" %}
+{% set elements = items %}
+", "modules/menu_item_extras/modules/mie_demo_base/templates/menu--extras.html.twig", "/var/www/html/foxnews/web/modules/menu_item_extras/modules/mie_demo_base/templates/menu--extras.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 24);
+        static $filters = array();
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set'],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
---
+++ web/sites/default/files/php/twig/61f2e2c26fb5f_region--footer-social-ico_IvwRVjHSMY1zqz09YOukJR2Z1/894L-xKvagyI4WqC5VFh659bFi9VPH9gbP5vlIvBA80.php
@@ -0,0 +1,127 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/foxnews/template/footer/region--footer-social-icon.html.twig */
+class __TwigTemplate_c8e99657c67fa0de6b409536a80ff48202997cb3d3e69bd8956303f2507c1811 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453 = $this->extensions["Drupal\\webprofiler\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453->enter($__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "themes/custom/foxnews/template/footer/region--footer-social-icon.html.twig"));
+
+        // line 17
+        if (($context["content"] ?? null)) {
+            // line 18
+            echo "  <div";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => "social-icon"], "method", false, false, true, 18), 18, $this->source), "html", null, true);
+            echo ">
+    ";
+            // line 19
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["content"] ?? null), 19, $this->source), "html", null, true);
+            echo "
+  </div>
+";
+        }
+        
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453->leave($__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/foxnews/template/footer/region--footer-social-icon.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  49 => 19,  44 => 18,  42 => 17,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Default theme implementation to display a region.
+ *
+ * Available variables:
+ * - content: The content for this region, typically blocks.
+ * - attributes: HTML attributes for the region <div>.
+ * - region: The name of the region variable as defined in the theme's
+ *   .info.yml file.
+ *
+ * @see template_preprocess_region()
+ *
+ * @ingroup themeable
+ */
+#}
+{% if content %}
+  <div{{ attributes.addClass('social-icon') }}>
+    {{ content }}
+  </div>
+{% endif %}
+", "themes/custom/foxnews/template/footer/region--footer-social-icon.html.twig", "/var/www/html/foxnews/web/themes/custom/foxnews/template/footer/region--footer-social-icon.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("if" => 17);
+        static $filters = array("escape" => 18);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
