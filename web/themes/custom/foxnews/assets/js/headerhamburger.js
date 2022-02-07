2aa6cc31ccd6696eb9909326273b941d44247d3b
JatinGupta40 <gupta.jatin40@gmail.com>
2022-02-07 14:23:35 +0530

    #2-node-issue-hamburger

--- web/themes/custom/foxnews/assets/js/headerhamburger.js
+++ web/themes/custom/foxnews/assets/js/headerhamburger.js
@@ -4,12 +4,16 @@ document.querySelector('.hamburgericon').addEventListener('click', () => {
   {
     // Toggle/click to open.
     document.querySelector('.hamburger').classList.add('hamburgertogglemenu');
+    document.querySelector('.searchHam').classList.add('searchHamburger');
+    document.querySelector('.hamburgerfooter').classList.add('hamburgerFooter');
     flag = true;
   }
   else
   {
     // Toggle/click to close.
     document.querySelector('.hamburger').classList.remove('hamburgertogglemenu');
+    document.querySelector('.searchHam').classList.remove('searchHamburger');
+    document.querySelector('.hamburgerfooter').classList.remove('hamburgerFooter');
     flag = false;
   }
 });
