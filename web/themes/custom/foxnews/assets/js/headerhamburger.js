d4d067ae4720efe49468fcb47286b5ed4f33b624
JatinGupta40 <gupta.jatin40@gmail.com>
2022-03-03 16:58:42 +0530

    #1 #2 Header and Footer

--- web/themes/custom/foxnews/assets/js/headerhamburger.js
+++ web/themes/custom/foxnews/assets/js/headerhamburger.js
@@ -1,8 +1,19 @@
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
+flag = false;
+document.querySelector('.hamburger').addEventListener('click', () => {
+  if (!flag) 
+  {
+    // Toggle/click to open.
+    document.querySelector('.hamburger').classList.add('hamburgertogglemenu');
+    document.querySelector('.searchHam').classList.add('searchHamburger');
+    document.querySelector('.hamburgerfooter').classList.add('hamburgerFooter');
+    flag = true;
   }
-}
+  else
+  {
+    // Toggle/click to close.
+    document.querySelector('.hamburger').classList.remove('hamburgertogglemenu');
+//    document.querySelector('.searchHam').classList.remove('searchHamburger');
+ //   document.querySelector('.hamburgerfooter').classList.remove('hamburgerFooter');
+    flag = false;
+  }
+});
