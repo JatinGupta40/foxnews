c5b97ad8a9aeb9c0d184014480ddd653db0a56a1
JatinGupta40 <gupta.jatin40@gmail.com>
2022-03-19 23:34:35 +0530

    8-Foxnation Second RightSide Section Done

--- web/themes/custom/foxnews/assets/gulpfile.js
+++ web/themes/custom/foxnews/assets/gulpfile.js
@@ -3,12 +3,13 @@ var sass = require('gulp-sass')(require('sass'));
 
 // Task 1 : Coverting SASS to CSS
 gulp.task('sass', async function() {
-   return gulp.src('./sass/*.scss')
+   return gulp.src('./sass/*/style.scss')
    .pipe(sass())
    .pipe(gulp.dest('./css'));
 });
 
 // Task 2 : Watch Task
 gulp.task('watch', async function() {
-   gulp.watch('./sass/*.scss', gulp.series('sass'));
+   gulp.watch('./sass/**/style.scss', gulp.series('sass'));
 })
+
